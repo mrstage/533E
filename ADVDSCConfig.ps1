4249b533fe27a623c7239042c94f32a3da4e8fd0
mrstage <rstagg45@hotmail.com>
2018-06-10 01:01:26 +0100

    Add files via upload

---
+++ ADVDSCConfig.ps1
@@ -0,0 +1,23 @@
+﻿configuration ADVDSCConfig
+{
+    Node Web-Server
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Present'
+            Name                 = 'Web-Server'
+            IncludeAllSubFeature = $true
+
+        }
+    }
+
+    Node NonWebServer
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Absent'
+            Name                 = 'Web-Server'
+
+        }
+    }
+    }
